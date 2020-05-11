ca0642593b473f3c80a4bd9301e055484ce2feb7
Mark Lee <markcmlee@gmail.com>
2020-05-11 00:00:33 -0400

    game works

--- client/components/InputField.jsx
+++ client/components/InputField.jsx
@@ -4,46 +4,85 @@ import Results from './Results.jsx'
 
 // calculatewpm = (typedCharacters/5) / endTime-startTime          *          60seconds / endTime-startTime
 
-//we have content, we know what the string that we are checking
-// have a pointer of some sort? State?  and if content[pointer] === e.target.value pointer ++ and if wrong
-// make sure you have to correct your mistake before being allowed to keep going.
 
-// need to reset letterpointer if we choose to choose a new subject, mid-type or we prevent changing stuff when we start the current snippet
 
 
 
 const InputField = props => {
-  const [letterPointer, setLetterPointer] = useState(0);
-  const [wordPointer, setWordPointer] = useState(0);
-  const [wordsTyped, setWordsTyped] = useState('');
-  const [fullString, setFullString] = useState('');
   const [startTime, setStartTime] = useState(0);
-  const [endTime, setEndTime] = useState(0)
   const [wordsPerMinute, setWordsPerMinute] = useState(0);
+  const [completedWords, setCompletedWords] = useState([]);
+  const [snippetSpace, setSnippetSpace] = useState([]);
+  const [snippetProp, setSnippetProp] = useState('');
 
-
-  const checkForErrors = (letter) => {
-    let letterSplit = props.content.content.split('');
-    let wordArray = props.content.content.split(' ');
-    
-    if (letter === letterSplit[letterPointer]) {
-      setWordsTyped(prevString => prevString += letter);
-      setLetterPointer(prevCount => prevCount + 1);
-      calculateWPM(e)
-      if (letterPointer === letterSplit.length - 1) {
-        setLetterPointer(prevCount => prevCount = 0);
-      }
-    } else {
-      
-    }
-    console.log('words typed', wordsTyped);
-    // console.log('currentletter', letter);
-    console.log('split letters', letterSplit);
-    // console.log('word array', wordArray);
-    console.log('letterPointer', letterPointer);
+ 
+  const resetState = () => {
+    setStartTime(0)
+    setWordsPerMinute(0)
+    setCompletedWords([])
+    setSnippetSpace([])
+    setSnippetProp('')
+    console.log('You win!')
   }
 
-  //establishes start time upon entering into the text box. 
+  useEffect(() => {
+    if (snippetSpace.length === 0 && props.content.content)
+    {
+      setSnippetSpace(space=>space=props.content.content.split(" "))
+      setSnippetProp(snip => snip = props.content.content )
+      // document.querySelector('#textInput').value = '';
+    }
+  })
+  
+  if (props.content.content) {
+    // console.log('snippetSpace', snippetSpace)
+    // console.log('split contents', props.content.content.split(' '))
+    if (snippetProp != props.content.content){
+      setSnippetSpace(space=>space=props.content.content.split(" "))
+      setSnippetProp(snip => snip = props.content.content )
+    }
+  }
+
+
+  const checkForErrors = (event) => {
+    // we won't care about tabbing.
+    console.log("we are doing something")
+    let snippetWords = snippetSpace ////,//////////////////snippet.split(' ')
+    console.log("Snippet words", snippetWords)
+    let wholeWord = event.target.value;
+    console.log("wholeWord",wholeWord)
+    let lastInput = wholeWord[wholeWord.length-1]
+    console.log('lastInput', lastInput)
+
+  
+    const currentWord = snippetWords[0]
+    console.log("current word",currentWord)
+    if (lastInput === ' ' || lastInput === "\n"){
+      console.log("We got a match")
+      console.log(wholeWord.trim(),"===", currentWord)
+      if (wholeWord.trim() === currentWord.trim()){
+        console.log("WE MATCHED OUR TRIM")
+        let remainingWords = [...snippetWords.slice(1)]
+        console.log(remainingWords)
+          if (remainingWords.length === 0) {
+            event.target.value = ''
+            return resetState();
+          }
+        //let finishedWords = [...completedWords,currentWord]
+        event.target.value = ''
+        setSnippetSpace(remainingWords)
+      }
+      else {
+        event.target.value = wholeWord.trim()
+      }
+    }
+    else{
+      // update wholeWord
+      //update lastInput
+    
+    }
+  
+  }
 
   const startRace = () => {
     if (startTime === 0) {
@@ -66,9 +105,9 @@ const InputField = props => {
 
   return (
     <div className='inputContainer'>
-      <textarea id='textInput' placeholder="Click Here to Start The CODERACE" 
+      <textarea id='textInput' placeholder="Click Here to Start The CODERACE"
                 onFocus ={startRace} 
-                onInput={(e)=> { checkForErrors(e.target.value.slice(-1)) }} 
+                onInput={(e)=> { checkForErrors(e)/*checkForErrors(e.target.value.slice(-1))*/; calculateWPM(e) }} 
       ></textarea>
 
       <p id='currentWPM'>
@@ -81,62 +120,5 @@ const InputField = props => {
     </div>
   )
 }
-
-
-
-
-// class InputField extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       splitContent: [],
-//       input: '',
-//       pointer: 0,
-//     };
-//     this.checkForErrors = this.checkForErrors.bind(this)
-//     this.pointer = 0;
-//   }
-
-//   checkForErrors(letter) {
-//     const split = this.props.content.content.split('');
-//     const split2 = this.props.content.content.split(' ')
-//     this.setState({ splitContent: split })
-//     const inputArray = '';
-//     // if (letter == this.state.splitContent[this.state.pointer] ) {
-//     //   this.setState({ pointer: this.state.pointer++ })
-//     //   this.setState({ input: this.state.input += letter })
-//     // }
-//     if (letter == split[this.pointer]) {
-//       // this.setState({ pointer: this.state.pointer++ })
-//       this.pointer += 1;
-//       // this.setState({ input: this.state.input += letter })
-//     }
-//     // console.log(this.state.input)
-//     console.log(letter)
-//     console.log(split)
-//     console.log(split2)
-//     console.log(this.pointer)
-//     // console.log(this.state.pointer)
-//   }
-
-//   render() {
-//     console.log('state', this.props.content.content)
-//     return (
-//       <div className='inputContainer'>
-//         {/* <input id='textInput' type='textbox' placeholder="This is where people type stuff" onInput={(e)=> (console.log(e.target.value.slice(-1)))} ></input> */}
-//         {/* <textarea id='textInput' placeholder="This is where people type stuff" onInput={(e)=> (console.log(e.target.value.slice(-1)))}></textarea> */}
-//         <textarea id='textInput' placeholder="This is where people type stuff" onInput={(e)=> (this.checkForErrors(e.target.value.slice(-1)))} ></textarea>
-//         <p id='currentWPM'>
-//           {/* Current WPM */}
-//           95
-//         </p>
-        
-//           < Results content={ this.props.content }/>
-  
-//       </div>
-//     )
-//   }
-// }
-
 
 export default InputField;
